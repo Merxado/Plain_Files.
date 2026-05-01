16c7021e46d73287477026c25d48c4152d0150f0
Juan Zuluaga <juan.zuluaga@empactit.com>
2026-04-30 19:30:06 -0500

    Simple text file

---
+++ UI/Program.cs
@@ -0,0 +1,52 @@
+﻿using Core;
+
+var textFile = new SimpleTextFile("c:\\tmp\\animals.txt");
+var lines = textFile.ReadLines();
+var list = lines.ToList();
+var option = string.Empty;
+
+do
+{
+    option = Menu();
+    switch (option)
+    {
+        case "1":
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            break;
+        case "2":
+            Console.Write("Enter a new line: ");
+            var newLine = Console.ReadLine();
+            list.Add(newLine!);
+            break;
+        case "3":
+            Console.Write("Enter a new line: ");
+            var lineToRemove = Console.ReadLine();
+            list.Remove(lineToRemove!);
+            break;
+        case "6":
+            textFile.WriteLines(list.ToArray());
+            Console.WriteLine("Changes saved.");
+            break;
+        default:
+            break;
+    }
+} while (option != "0");
+textFile.WriteLines(list.ToArray());
+Console.WriteLine("Changes saved.");
+
+string Menu()
+{
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1. Show lines");
+    Console.WriteLine("2. Add line");
+    Console.WriteLine("3. Remove one occurrence");
+    Console.WriteLine("4. Remove all occurrences"); // Homework: Implement this option
+    Console.WriteLine("5. Sort"); // Homework: Implement this option
+    Console.WriteLine("6. Save changes");
+    Console.WriteLine("0. Exit");
+    Console.Write("Choose an option: ");
+    return Console.ReadLine() ?? string.Empty;
+}
